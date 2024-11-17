c81c62de028cc45a9ce9ce24c765b1fa235124b2
IO group <service@iogoods.de>
2024-11-17 18:48:48 +0100

    Update EnigmaCracker.py

--- EnigmaCracker.py
+++ EnigmaCracker.py
@@ -89,7 +89,7 @@ async def check_btc_balance_async(address, session):
         "jsonrpc": "2.0",
         "method": "blockchain.address.get_balance",
         "params": [address],
-        "id": None
+        "id": 1
     }
     logger.info(f"Prüfe Balance für Adresse: {address}")
     
